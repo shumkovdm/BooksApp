3c63a452079f49aa0f30bbfe2d67fc86bcc6fe8a
admin <admin@LAPTOP-BCTN3QAV>
2024-11-15 16:07:15 +0500

    Добавьте файлы проекта.

---
+++ BooksApp/BooksViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace BooksApp.ViewModels
+{
+    public class BooksViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Book> books;
+        private Book selectedBook;
+
+        public ObservableCollection<Book> Books
+        {
+            get { return books; }
+            set
+            {
+                books = value;
+                OnPropertyChanged("Books");
+            }
+        }
+
+        public Book SelectedBook
+        {
+            get { return selectedBook; }
+            set
+            {
+                selectedBook = value;
+                OnPropertyChanged("SelectedBook");
+            }
+        }
+
+        public BooksViewModel()
+        {
+            Books = new ObservableCollection<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            Books.Add(book);
+        }
+
+        public void UpdateBook(Book oldBook, Book newBook)
+        {
+            int index = Books.IndexOf(oldBook);
+            if (index != -1)
+                Books[index] = newBook;
+        }
+
+        public void RemoveBook(Book book)
+        {
+            Books.Remove(book);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ BooksApp/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using BooksApp.ViewModels;
+using System.Windows;
+
+namespace BooksApp
+{
+    public partial class MainWindow : Window
+    {
+        public BooksViewModel ViewModel { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = new BooksViewModel();
+            DataContext = ViewModel;
+        }
+
+        private void AddBookButton_Click(object sender, RoutedEventArgs e)
+        {
+            var bookEditor = new BookEditorWindow(new Book());
+            if (bookEditor.ShowDialog() == true)
+            {
+                ViewModel.AddBook(bookEditor.Book);
+            }
+        }
+
+        private void EditBookButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ViewModel.SelectedBook != null)
+            {
+                var bookEditor = new BookEditorWindow(ViewModel.SelectedBook);
+                if (bookEditor.ShowDialog() == true)
+                {
+                    ViewModel.UpdateBook(ViewModel.SelectedBook, bookEditor.Book);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выбери книгу для редактирования.", "Ошибка =/", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void RemoveBookButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ViewModel.SelectedBook != null)
+            {
+                ViewModel.RemoveBook(ViewModel.SelectedBook);
+            }
+            else
+            {
+                MessageBox.Show("Выбери книгу для удаления.", "Ошибка =(", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
